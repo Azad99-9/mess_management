5b8ce59eeebe4bbf0ba27fd6474c240d436a6b08
Azad99-9 <shaikazad2003@gmail.com>
2024-12-09 13:13:15 +0530

    created pages

---
+++ lib/services/theme_service.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ThemeService {
+  static const Color primaryColor = Color(0xff2962FF);
+  static const Color primaryAccent = Color(0xfff5fbff);
+  static const Color secondaryColor = Colors.white;
+
+}
