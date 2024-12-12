88962f0e7de1d4f20b7420bcc5e446b5b1e0b404
sayyad-asifbasha <asifbasha4873@gmail.com>
2024-12-12 22:16:14 +0530

    add notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:mess_management/constants/routes.dart';
 import 'package:mess_management/firebase_options.dart';
@@ -17,10 +18,18 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   setUpLocator();
   runApp(const MyApp());
+  notificationService.requestNotificationPermission();
+  notificationService.getToken();
+  notificationService.FirebaseInit();
 }
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message)async {
+  await Firebase.initializeApp();
 
+}
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
